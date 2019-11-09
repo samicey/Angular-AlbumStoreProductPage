56600a9861adb899f59f64f464f5636f014fbba0
OBIKAONU SAMUEL <49449271+samicey@users.noreply.github.com>
2019-11-09 22:28:56 +0000

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,9 @@ import {Http, Response} from '@angular/http';
 
 import 'rxjs/add/operator/map';
 
+import {Album} from './album';
+import{Observable} from 'rxjs/observable';
+
 @Injectable()
 export class ProductService {
 
@@ -10,8 +13,8 @@ private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response)=>response.json())
+  getAlbum(id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json()); 
   }
 
 }
