7503246984a0e44e1dbc1f155623c6f99a8a12ab
OBIKAONU SAMUEL <49449271+samicey@users.noreply.github.com>
2019-11-09 03:59:32 +0000

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -6,7 +6,26 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-private _albumUrl = '../assets/album.json';
+private _albumUrl = '../asimport { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+  albumInfo;
+  constructor(private _productService: ProductService) { 
+    
+  }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response=>this.albumInfo = response)
+  }
+
+}
+sets/album.json';
 
   constructor(private _http: Http) { }
 
